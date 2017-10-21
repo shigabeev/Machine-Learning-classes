f4c0d96069de53f2c2550f947ef6a8b78b035632
shigabeev <beat@live.ru>
2017-10-21 19:16:33 +0300

    converted into library

--- least_squares.py
+++ least_squares.py
@@ -1,35 +1,23 @@
+# let it be library for solving machine learning problems
 
-# coding: utf-8
-
-# In[1]:
-
-import random
 import numpy as np
+import matplotlib.pyplot as plt
 
+def solve(X, y):
+    C = np.dot(np.transpose(X),X)
+    G = np.linalg.inv(C)
 
-# In[2]:
+    B = np.dot(np.dot(G, np.transpose(X)),y)
+    return B
 
-# generate x and Y values for y=2x+5+E
-l = 26
-x0 = np.array([1 for _ in range(l)])
-x1 = np.array([x/5 for x in range(l)]) # from 0 to 5, step 0.2
-X = np.array([[x0[i], x1[i]] for i in range(len(x1))])
-Y = np.array([2*x+5+random.random()-1 for x in x1])
-
-print(X)
-print(Y)
-
-
-# In[3]:
-
-C = np.dot(np.transpose(X),X)
-G = np.linalg.inv(C)
-
-B = np.dot(np.dot(G, np.transpose(X)),Y)
-print(B) # should be somewhere close to 5 and 2
-
-
-# In[ ]:
-
-
-
+def show(X, Y, B):
+    # Warning! It works only for 1D of x
+    plt.figure()
+    # show train data first 
+    plt.scatter(X[:,1], y=Y)
+    # then plot prediction line
+    X_line, Y_line = X[:, 1], np.dot(X,B)
+    xs = [X_line[0], X_line[-1]]
+    ys = [Y_line[0], Y_line[-1]]
+    plt.plot(xs, ys)
+    plt.show()
