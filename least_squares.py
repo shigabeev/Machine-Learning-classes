633505928fc51a3406dede6b859997e154b93ee6
shigabeev <beat@live.ru>
2017-10-26 02:19:57 +0300

    readability +

--- least_squares.py
+++ least_squares.py
@@ -4,12 +4,114 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
+class LeastSquares:
+    def __init__(self, constant=False, pow_2_factors = None):
+        self.constant = constant
+        self.pow_2_factors = pow_2_factors
+        pass
+    
+    def fit(self, X, y):
+        '''
+        Fit Least Squares regression model 
+        
+        using formula B = (X^T * X)^(-1) * X^T * Y
+
+        Parameters
+        ----------
+        X - matrix of features of size (m x n), [[x_11, x_21, ... , x_m1]
+                                                 [x_12, x_22, ... , x_m2]
+                                                              ...
+                                                 [x_1n, x_2n, ... , x_mn]]
+        y - array of true outputs
+        
+        Returns
+        -------
+        self : returns an instance of self.
+        '''
+        self.X=X
+        self.y=y
+        if self.constant:
+            X = self.add_constant(X)
+        for factor in self.pow_2_factors:
+            X = self.add_squared(X, factor)
+        C = np.dot(np.transpose(X), X)
+        G = np.linalg.inv(C)
+
+        self.B = np.dot(np.dot(G, np.transpose(X)),y)
+
+    def predict(self, X):
+        '''
+        Predicts outputs based on X and model
+        '''
+        Y = []
+        if isinstance(X[0], list):
+            y = 0
+            for line in X:
+                for i, x in enumerate(line):
+                    y = y + x*self.B[i]
+            Y.append(y)
+        else:
+            Y = [x*self.B[0] for x in X]
+        return Y
+
+    def solve_(self, X, y):
+        
+        C = np.dot(np.transpose(X),X)
+        G = np.linalg.inv(C)
+
+        B = np.dot(np.dot(G, np.transpose(X)),y)
+        return B
+
+    def add_constant(self, X):
+        '''
+        Adds row of ones to matrix X
+        '''
+        if isinstance(X[0], list):
+            return [[1.] + x for x in X]
+        else:
+            return [[1, x] for x in X]
+
+    def add_squared(self, X, factor):
+        '''
+        Adds square of factor to matrix X
+        '''
+        print(X[0])
+        if isinstance(X[0], list):
+            return [[float(k) for k in x[:]] + x[factor]**2 for x in X]
+        else:
+            return [[x, x**2] for x in X]
+        
+def plot_errors(Y_True, Y_pred):
+    E = []
+    assert(Y_pred == Y_pred)
+    for i, _ in enumerate(Y_True):
+        E.append(Y_True[i]-Y_pred[i])
+    plt.figure()
+    plt.scatter(x = Y_True, y=E)
+    plt.show()
+    return E
+
 def solve(X, y):
     C = np.dot(np.transpose(X),X)
     G = np.linalg.inv(C)
 
     B = np.dot(np.dot(G, np.transpose(X)),y)
     return B
+
+def generate_data(X, Y, ratio = 0.9):
+    '''
+    Splits data in two according to ratio into train data and test data
+    '''
+    from random import shuffle
+    corpus = [[X[i]]+[Y[i]] for i, _ in enumerate(Y)]
+    shuffle(corpus)
+    train_amount = int(len(corpus)*ratio)
+    train_target = [corpus[i][1] for i in range(train_amount)]
+    train_data = [corpus[i][0] for i in range(train_amount)]
+    test_target = [corpus[i][1] for i in range(train_amount, len(corpus))]
+    test_data = [corpus[i][0] for i in range(train_amount, len(corpus))]
+    return train_data, train_target, test_data, test_target
+
 
 def show(X, Y, B, dims=[1]):
     for dim in dims:
@@ -44,7 +146,28 @@ def open_csv(filename, delimiter = ","):
     return features, titles
 
 if __name__ == "__main__":
-    X = [[1, 2, 3, 4],
-         [2, 3, 5, 8],
-         [4, 7, 14, 1]]
-    print(normalize(X))
+    # one-dimensional
+    features, titles = open_csv("car_price/data.csv", delimiter=';')
+    X = [float(x[0]) for x in features]
+    Y = [float(x[-1]) for x in features]
+    del features, titles
+    
+    train_data, train_target, test_data, test_target = generate_data(X, Y)
+
+    clf = LeastSquares(constant=True, pow_2_factors=[])
+    clf.fit(train_data, train_target)
+    
+    plot_errors(test_target, clf.predict(test_data))
+
+    '''
+    features, titles = open_csv("car_price/data.csv", delimiter=';')
+    X = [[1.] + [float(k) for k in x[:5]] + [float(x[1])**2] for x in features]  # + [float(x[7])]
+    Y = [float(x[-1]) for x in features]
+
+    train_data, train_target, test_data, test_target = generate_data(X, Y)
+
+    clf = LeastSquares()
+    clf.fit(train_data, train_target)
+    
+    plot_errors(test_target, clf.predict(test_data))
+    '''
