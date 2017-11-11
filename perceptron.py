58072ef7c3c08319971534b2bec4f6e563b55881
shigabeev <beat@live.ru>
2017-11-11 21:32:51 +0300

    fully working perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import random
 
 def neuron_output(inputs, weights):
     return sigmoid(np.dot(inputs, weights))
@@ -21,7 +22,7 @@ def feed_forward(neural_network, input_vector):
 
     for layer in neural_network:
 
-        input_with_bias = input_vector + [1]             # add a bias input
+        input_with_bias = list(input_vector) + [1., ]            # add a bias input
         output = [neuron_output(neuron, input_with_bias) # compute the output
                   for neuron in layer]                   # for this layer
         outputs.append(output)                           # and remember it
@@ -31,9 +32,9 @@ def feed_forward(neural_network, input_vector):
 
     return outputs
 
-def backpropagate(network, input_vector, target):
+def backpropagate(network, input_vector, target, lrate = 0.01):
 
-    hidden_outputs, outputs = feed_forward(network, input_vector)
+    hidden_outputs, outputs = feed_forward(network, input_vector) # really, only 2 layers !
 
     # the output * (1 - output) is from the derivative of sigmoid
     output_deltas = [output * (1 - output) * (output - target[i])
@@ -42,7 +43,7 @@ def backpropagate(network, input_vector, target):
     # adjust weights for output layer (network[-1])
     for i, output_neuron in enumerate(network[-1]):
         for j, hidden_output in enumerate(hidden_outputs + [1]):
-            output_neuron[j] -= output_deltas[i] * hidden_output
+            output_neuron[j] -= output_deltas[i] * hidden_output * lrate
 
     # back-propagate errors to hidden layer
     hidden_deltas = [hidden_output * (1 - hidden_output) *
@@ -52,29 +53,57 @@ def backpropagate(network, input_vector, target):
     # adjust weights for hidden layer (network[0])
     for i, hidden_neuron in enumerate(network[0]):
         for j, input in enumerate(input_vector + [1]):
-            hidden_neuron[j] -= hidden_deltas[i] * input
+            hidden_neuron[j] -= hidden_deltas[i] * input * lrate
 
 def predict(network, input):
     result = feed_forward(network, input)[-1]
     return result.index(max(result))
 
+def generate_network(input_size, num_hidden, output_size):
+    # each hidden neuron has one weight per input, plus a bias weight
+    hidden_layer = [[random.random() for __ in range(input_size + 1)]
+                    for __ in range(num_hidden)]
+
+    # each output neuron has one weight per hidden neuron, plus a bias weight
+    output_layer = [[random.random() for __ in range(num_hidden + 1)]
+                    for __ in range(output_size)]
+
+    # the network starts out with random weights
+    network = [hidden_layer, output_layer]
+    return network
+
 if __name__ == "__main__":
+    from sklearn.datasets import load_iris
+    import matplotlib.pyplot as plt
+
+    data = load_iris()
+    
+    network = generate_network(4, 8, 3)
+
+    # Learning stage
+
+    for _ in range(1000):       # 1000 epochs seems enough to converge
+        for i, inputs in enumerate(data.data[:]):
+            backpropagate(network, inputs, np.eye(3)[data.target[i]])
+
+    # Testing
+    for i, inputs in enumerate(data.data[:]):
+        print("Predicted: ", predict(network, inputs), "Target: ", data.target[i])
+   
+   
+    # XOR example
+   
     # in1, in2 = int(input()), int(input()) # inputs for our neural network
 
     # weights for network
 
-    XOR_nw = [[[1, 0.5, 0.3], #n1 neuron
-               [0.1, 0.2, 1]],#n2 neuron
-               # output layer
-               [[1, 0.5, 0.3], #n1 neuron
-               [0.1, 0.2, 1]]  #o2 neuron
-               ]
+    network = generate_network(2, 3, 2)
 
     for i in range(1000):
         for x in [0, 1]:
             for y in [0, 1]:
-                backpropagate(XOR_nw, [x, y], [1, 0] if x==y else [0, 1])
+                backpropagate(network, [x, y], [1, 0] if x==y else [0, 1])
 
     for x in [0, 1]:
         for y in [0, 1]:
-            print("[%s, %s]" %(x, y), predict(XOR_nw, [x, y]))
+            print("[%s, %s]" %(x, y), predict(network, [x, y]))
